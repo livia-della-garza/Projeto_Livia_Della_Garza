48bb0e82eee44a5ace146c7c5e15773398dcae78
Lívia Della Garza <liviadgarza@gmail.com>
2024-10-25 17:49:08 -0300

    Classes A e B adicionadas

---
+++ classeA.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class A{
+	private:
+		int A1;
+		float A2;
+	
+	public:
+		int getA1(){
+			return A1;
+		}
+		
+		float getA2(){
+			return A2;
+		}
+		
+		void setA1(int novoA1){
+			A1 = novoA1;
+		}
+		
+		void setA2(float novoA2){
+			A2 = novoA2;
+		}
+		
+		void MA1(){
+			cout << "MA1" << endl;
+		}
+		
+		void MA2(){
+			cout << "MA2" << endl;
+		}
+};
---
+++ classeB.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class B{
+	private:
+		int B1;
+		float B2;
+	
+	public:
+		int getB1(){
+			return B1;
+		}
+		
+		float getB2(){
+			return B2;
+		}
+		
+		void setB1(int novoB1){
+			B1 = novoB1;
+		}
+		
+		void setB2(float novoB2){
+			B2 = novoB2;
+		}
+		
+		void MB1(){
+			cout << "MB1" << endl;
+		}
+		
+		void MB2(){
+			cout << "MB2" << endl;
+		}
+};
