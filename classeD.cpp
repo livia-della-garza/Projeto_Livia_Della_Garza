249c17e5c8cc6febc9a5fdade0a511b68c4f67cc
Lívia Della Garza <liviadgarza@gmail.com>
2024-10-25 20:24:58 -0300

    Criação da classe D

---
+++ classeD.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class D{
+	private:
+		int D1;
+		float D2;
+	
+	public:
+		void MD1(){
+			cout << "MD1" << endl;
+		}
+		
+		void MD2(){
+			cout << "MD2" << endl;
+		}
+};
