92e696303d612fd9073541921830f6cd426044cd
Lívia Della Garza <liviadgarza@gmail.com>
2024-10-25 19:33:15 -0300

    Criação da Classe C

---
+++ classeC.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class C{
+	private:
+		string C1; 
+		int C2;
+		
+	public:
+		void MC1(){
+			cout << "MC1" << endl;
+		}
+		
+		void MC2(){
+			cout << "MC2" << endl;
+		}
+};
